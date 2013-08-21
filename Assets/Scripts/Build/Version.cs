f2e65eac58de27c60cca146c1bc80940f4a0d090
Leandro Casanova <casanova.leandro@gmail.com>
2013-08-21 00:01:10 -0300

    Added version info scripts

---
+++ Assets/Scripts/Build/Version.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Version : MonoBehaviour {
+
+	public TextAsset textFile;
+	
+	private GUIText guiTextInfo;
+	
+	void Awake()
+	{
+		guiTextInfo = GetComponent<GUIText>();
+		guiTextInfo.text = textFile.ToString();
+	}
+	
+}
